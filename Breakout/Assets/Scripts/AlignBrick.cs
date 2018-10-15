02f2954b81dc724a397b21f414742167daf3ff65
Gunto <trent.braden.williams@gmail.com>
2018-10-15 16:53:24 +1100

    Working version of auto layout

--- Breakout/Assets/Scripts/AlignBrick.cs
+++ Breakout/Assets/Scripts/AlignBrick.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AlignBrick : MonoBehaviour {
+public class AlignBrick : MonoBehaviour
+{
 
-    public RectTransform rect;
+    private Transform layoutTransform;
     private Vector3 rectPos;
     private HorizontalLayoutGroup layout;
 
-	// Use this for initialization
-	void Start () {
-        //layout = rect.gameObject.GetComponentInParent<HorizontalLayoutGroup>();
-        //layout.enabled = false;
-        rectPos = rect.transform.position;
+    public Transform LayoutTransform
+    {
+        get
+        {
+            return layoutTransform;
+        }
+
+        set
+        {
+            layoutTransform = value;
+        }
+    }
+
+    // Use this for initialization
+    public void Align()
+    {
+        rectPos = LayoutTransform.position;
         Debug.Log(rectPos);
         transform.position = rectPos;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    }
+}	
--- Breakout/Assets/Scripts/CreateBricks.cs
+++ Breakout/Assets/Scripts/CreateBricks.cs
@@ -5,47 +5,79 @@ using UnityEngine;
 public class CreateBricks : MonoBehaviour {
 
     public Sprite sprite;
-    public float width, height;
     private GameObject brick;
+    private GameObject vertical;
+    private GameObject horizontal;
+    public GameObject layout;
     private SpriteRenderer spRen;
-    private Vector2 scale;
-    private Vector3 position;
-    private Vector3 screenPosition;
-    private Camera cam;
-
-    public int margin; //Gap from screen edge
-    public int padding; //Gap in between bricks
+    public Vector2 scale;
+    private Transform[] row;
+    private Transform[] column;
+    public float scaleAmount = 1f;
 
 	// Use this for initialization
 	void Start ()
     {
-        cam = Camera.main;
-        scale = new Vector2(width, height);
-        screenPosition = new Vector3(0, Screen.height * 4/5);
-        position = cam.ScreenToWorldPoint(screenPosition);
-        CreateSprite();
-        Debug.Log(Screen.height);
-	}
+        column = new Transform[layout.transform.childCount];
+        for (int i = 0; i < layout.transform.childCount; i++)
+        {
+            column[i] = layout.transform.GetChild(i);
+        }
+        row = layout.transform.GetChild(0).GetComponentsInChildren<Transform>();
+        vertical = new GameObject
+        {
+            name = "Bricks Group"
+        };
+        CreateSets();
+        layout.SetActive(false); //Disable UI used for layout
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    private void CreateSets()
+    {
+        for (int i = 0; i < column.Length; i++)
+        {
+            horizontal = new GameObject
+            {
+                name = "Row " + (i + 1)
+            };
+            horizontal.transform.SetParent(vertical.transform, true); //Parent under Bricks Group
+            horizontal.transform.position = column[0].transform.position; //Set initial position to match row used
+            CreateRows();
+            horizontal.transform.position = column[i].transform.position; //Reposition row to match BUG: Moves child transform as well
+        }
+    }
+
+    private void CreateRows()
+    {
+        for (int i = 1; i < row.Length; i++)
+        {
+            CreateSprite();
+            AlignBricks(row[i]);
+        }
+    }
+
     private void CreateSprite()
     {
         brick = new GameObject();
+        brick.transform.SetParent(horizontal.transform, true);
         brick.AddComponent<SpriteRenderer>();
+        //brick.AddComponent<BrickCollision>();
         spRen = brick.GetComponent<SpriteRenderer>();
         spRen.sprite = sprite;
         spRen.drawMode = SpriteDrawMode.Sliced;
         spRen.size = scale;
-        brick.transform.position = new Vector2(position.x + scale.x / 2, position.y);
     }
 
-    private void LayoutBrickRow()
+    private void AlignBricks(Transform position)
     {
-
+        brick.AddComponent<AlignBrick>();
+        brick.GetComponent<AlignBrick>().LayoutTransform = position;
+        brick.GetComponent<AlignBrick>().Align();
+        brick.transform.localScale = new Vector3(scaleAmount, scaleAmount, 1);
     }
-   
 }
