235822e815d913a9e9ed0b5341b395b29f4a41b1
Andre Lautee <102115854@student.swin.edu.au>
2018-10-19 21:59:14 +1100

    Add menu to game

---
+++ Breakout/Assets/Editor/MenuTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Editor
+{
+    class MenuTest
+    {
+        [Test]
+        public void PlayGameButtonChangesScene()
+        {
+            MenuController mc = GameObject.Find("MenuController").GetComponent<MenuController>();
+            mc.ToggleMenu();
+            bool expected = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Main";
+            bool actual = mc.InMenu;
+            Assert.AreEqual(expected, actual); 
+        }
+
+        [Test]
+        public void InstructionsButtonPressTest()
+        {
+            MenuController mc = GameObject.Find("MenuController").GetComponent<MenuController>();
+            mc.InstructionsButton();
+            bool actual = mc.InstructionsOpened;
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void ExitButtonPressTest()
+        {
+            MenuController mc = GameObject.Find("MenuController").GetComponent<MenuController>();
+            mc.ExitGame();
+            bool actual = mc.Exited;
+            Assert.IsTrue(actual);
+        }
+    }
+}
