2f93eb26b6cbde4c920ee0c52211584992a9149b
Zane Beveridge <101625501@student.swin.edu.au>
2018-10-22 22:20:12 +1100

    UI Controller Added with scoring

--- Breakout/Assets/Scripts/BrickController.cs
+++ Breakout/Assets/Scripts/BrickController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class BrickController : MonoBehaviour {
 
-	
+    public GameObject UIController;
 
     void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ball")
         {
             Destroy(gameObject);
+            UIController = GameObject.FindWithTag("UIController");
+            UIController.GetComponent<UIController>().IncrementScore();
         }
     }
 }
